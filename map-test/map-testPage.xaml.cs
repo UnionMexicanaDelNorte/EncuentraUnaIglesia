94204a40227b697b3d1f69aef41a25cc3fac83bf
icalderond <icalderond@outlook.com>
2018-02-21 20:22:20 -0600

    Agregando Nuget maps

---
+++ map-test/map-testPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace maptest
+{
+    public partial class map_testPage : ContentPage
+    {
+        public map_testPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
